1e818295e4325ff93f7560c7934699026e072a6a
AminduBhashana <waamidu@gmail.com>
2025-02-07 00:12:34 +0530

    final project

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Customer = require('./models/customer');
+const cors = require('cors');
+
 const app = express();
 
 mongoose.set('strictQuery', false);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(cors());
 
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
@@ -13,32 +17,113 @@ if(process.env.NODE_ENV !== 'production'){
 const PORT = process.env.PORT || 3000;
 const CONNECTION =  process.env.CONNECTION;
 
-const customers = [
-    {
-        "name" : "Saman",
-        "industry": "IT"
-    },
-    {
-        "name" : "kamal",
-        "industry": "Textile and Apparel"
-    },
-    {
-        "name" : "Nimal",
-        "industry": "Transport"
-    }
-];
-
 app.get('/', (req, res) => {
     res.send("Welcome!");
 });
 
-app.get('/api/customers', (req, res) => {
-    res.send({"Customers" : customers});
+app.get('/api/customers', async (req, res) => {
+    try{
+        const result = await Customer.find();
+        res.json({"customers" : result});
+    }catch (e) {
+        res.status(500).json({ error : e.message});
+    }  
 });
 
-app.post('/api/customers', (req, res) => {
+app.get('/api/customers/:id', async (req, res) =>{
+    try{
+        const {id: customerId} = req.params;
+        const customer = await Customer.findById(customerId);
+        if(!customer){
+            res.status(404).json({error: "User not found."});
+        }
+        else{
+            res.json({customer});
+        }   
+    }catch (e){
+        res.status(500).json({error: e.message});
+    }
+   
+});
+
+app.get('/api/orders/:id', async (req, res) =>{
+    try{
+        const {id: orderId} = req.params;
+        const result = await Customer.findOne({'orders._id': orderId});
+        if(!result){
+            res.status(404).json({error: "Order not found."});
+        }
+        else{
+            res.json({result});
+        }   
+    }catch (e){
+        res.status(500).json({error: e.message});
+    }
+   
+});
+
+
+app.put('/api/customers/:id', async (req, res) => {
+    try{
+        const customerId = req.params.id;
+        const result = await Customer.findOneAndReplace({_id: customerId}, req.body, { new: true});
+        res.json({updatedCustomer: result});
+    }
+    catch (e){
+        res.status(500).json({error: e.message});
+    }   
+});
+
+app.patch('/api/customers/:id',async (req, res) => {
+    try{
+        const customerId = req.params.id;
+        const customer = await Customer.findOneAndUpdate({_id: customerId}, req.body, { new: true});
+        res.json({customer});
+    }
+    catch (e){
+        res.status(500).json({error: e.message});
+    }   
+});
+
+app.patch('/api/orders/:id', async (req, res) => {
+    const orderId = req.params.id;
+    req.body._id = orderId;
+    try{
+        const result =  await Customer.findOneAndUpdate(
+            {'orders._id': orderId},
+            { $set: { 'orders.$': req.body }},
+            { new: true}
+        );
+        if(result){
+            res.json(result);
+        } else{
+            res.status(404).json({ error: e.message});
+        }
+    }
+    catch (e) {
+
+    }
+});
+
+app.post('/api/customers', async (req, res) => {
     console.log(req.body);
-    res.send(req.body);
+    const customer = new Customer(req.body);
+    try{
+        await customer.save();
+        res.status(201).json({customer});
+    }catch( error ){
+        res.status(400).json({error: error.message});
+    }
+});
+
+app.delete('/api/customers/:id', async (req, res) => {
+    try{
+        const customerId = req.params.id;
+        const result = await Customer.deleteOne({_id: customerId});
+        res.json({ deletedCount: result.deletedCount});
+    } catch (e){
+        res.status(500).json({ error : e.message});
+    }
 });
 
 const start = async () => {
---
+++ src/models/customer.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const customerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    industry: String,
+    orders: [{
+        description: String,
+        price: Number
+    }
+    ]
+});
+
+module.exports = mongoose.model('customers', customerSchema);
